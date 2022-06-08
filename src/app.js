643854a9b175a1e13804e55c4bd78f1c0b329611
naxinga <ngarconf@hotmail.com>
2022-06-08 19:14:18 +0000

    no me funcionan las funciones a los arrays

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['acordes','puedes',"azar"];
+var dom = [".es", ".com", ".ar"]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//function incluir (ar1,ar2){
+//  if ((ar1.includes( 's', -1) && ar2.includes( "s", -1)) && (ar1.includes( 'e', -2) && ar2.includes( "e", -2))){
+//    console.log(ar1);
+//    //console.log(i+i2+i3+domi);
+//}else{
+//    //console.log("Vas mal niño");
+//}
+//  return
+//}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (var i of pronoun) {
+  for (var i2 of adj){
+    for (var i3 of noun){
+      var fin = i3.substring(0,10)
+      for (var domi of dom){
+        var finDomi = domi.substring(1,10)
+        var a = fin[(fin.length)-1];
+        var b = fin[(fin.length)-2];
+        if ((fin.includes( a, -1) && finDomi.includes( a, -1)) && (fin.includes( b, -2) && finDomi.includes( b, -2))){
+          fin.reverse();
+          //console.log(fin);
+          //console.log(i+i2+i3+domi);
+        }else{
+          //console.log(i+i2+i3+domi);
+        }
+      }     
+  }
+ }
+}
