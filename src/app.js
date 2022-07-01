5a1c640cfb4f8e0aa5b1809cb8ae7b560bfab7b8
naxinga <104234469+naxinga@users.noreply.github.com>
2022-07-01 22:04:46 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,25 +3,25 @@ var adj = ['great', 'big' ];
 var noun = ['acordes','puedes',"azar"];
 var dom = [".es", ".com", ".ar"]
 
-for (var i of pronoun) {
-  for (var i2 of adj){
-    for (var i3 of noun){
-      var fin = i3.substring(0,10)
+for (var pronombre of pronoun) {
+  for (var adjetivo of adj){
+    for (var nombre of noun){
+      var fin = nombre.substring(0,10)
       for (var domi of dom){
         var finDomi = domi.substring(1,10)
         var a = fin[(fin.length)-1];
         var b = fin[(fin.length)-2];
         if ((fin.includes( a, -1) && finDomi.includes( a, -1)) && (fin.includes( b, -2) && finDomi.includes( b, -2))){
-          var probando = fin.split("");
-          probando.pop();
-          probando.pop();
-          var i6 = "";
-          for( i4 of probando){
-            var i6= i6 + i4;
+          var removeEnd = fin.split("");
+          removeEnd.pop();
+          removeEnd.pop();
+          var dominioFin = "";
+          for( index of removeEnd){
+            var dominioFin= dominioFin + index;
          }
-         console.log(i+i2+i6+domi);
+         console.log(pronombre+adjetivo+nombre+domi);
         }else{
-          console.log(i+i2+i3+domi);
+          console.log(pronombre+adjetivo+nombre+domi);
         }
       }     
   }
