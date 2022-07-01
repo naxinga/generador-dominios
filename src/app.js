5ab0451ff0742cc588cf483fd58431a495b8f496
naxinga <104234469+naxinga@users.noreply.github.com>
2022-07-01 21:59:48 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,6 @@ var adj = ['great', 'big' ];
 var noun = ['acordes','puedes',"azar"];
 var dom = [".es", ".com", ".ar"]
 
-//function incluir (ar1,ar2){
-//  if ((ar1.includes( 's', -1) && ar2.includes( "s", -1)) && (ar1.includes( 'e', -2) && ar2.includes( "e", -2))){
-//    console.log(ar1);
-//    //console.log(i+i2+i3+domi);
-//}else{
-//    //console.log("Vas mal niño");
-//}
-//  return
-//}
-
 for (var i of pronoun) {
   for (var i2 of adj){
     for (var i3 of noun){
